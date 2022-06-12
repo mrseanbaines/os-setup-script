a3c1a8906626b6b42da9058602808e17d6ccc8bb
Sean Baines <mrseanbaines@gmail.com>
2022-06-12 21:08:51 +0100

    WIP

--- os-setup-script.sh
+++ os-setup-script.sh
@@ -2,45 +2,71 @@
 
 # Setup script for setting up a new macOS machine
 
-echo "Starting setup"
+echo "Starting setup..."
 
-# Install Xcode CLI
+echo "Installing Xcode command line tools..."
+
+# Install Xcode command line tools
 xcode-select --install
 
 # Check for Homebrew to be present, install if it's missing
 if test ! $(which brew); then
   echo "Installing Homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  # Install Homebrew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# Update Homebrew recipes
+echo "Updating packages..."
+
+# Update Homebrew packages
 brew update
 
-# PACKAGES=(
-#   # List all the packages you want to install
-# )
+PACKAGES=(
+  # List all the packages you want to install
+  git
+  nvm
+  yarn
+)
 
-# echo "Installing packages..."
+echo "Installing packages..."
 
-# # Install Homebrew packages
-# brew install ${PACKAGES[@]}
+# Install Homebrew packages
+brew install ${PACKAGES[@]}
 
-# Any additional steps you want to add here
+# Install Node
+nvm install --latest-npm --lts
 
 echo "Cleaning up..."
 
 # Uninstall old versions of formulas
 brew cleanup
 
-# echo "Installing cask..."
+CASKS=(
+  # List all the applications you want to install
+  1password
+  authy
+  figma
+  firefox
+  gifox
+  google-chrome
+  insomnia
+  iterm2
+  linear-linear
+  notion
+  raycast
+  sketch
+  slack
+  spotify
+  tableplus
+  visual-studio-code
+  zoom
+)
 
-# CASKS=(
-#   # List all the applications you want to install
-# )
+echo "Installing cask apps..."
 
-# echo "Installing cask apps..."
-
-# brew install ${CASKS[@]} --cask
+# Install Homebrew casks
+brew install ${CASKS[@]} --cask
 
 # Set fast key repeat rate
 # defaults write NSGlobalDomain KeyRepeat -int 0
