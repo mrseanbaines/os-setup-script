10ccb5f6b5e5f41f6b7a65ee932c52f92b0a524f
Sean Baines <mrseanbaines@gmail.com>
2022-07-17 11:35:50 +0100

    Move password prompt to root script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,11 +2,24 @@
 
 echo "Starting setup..."
 
+# Ask for the administrator password upfront
+# sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished
+# while true; do
+#   sudo -n true
+#   sleep 60
+#   kill -0 "$$" || exit
+# done 2>/dev/null &
+
 # Configure MacBook preferences
 source mac-os.sh
 
 # Install Homebrew, update everything, install formulae & casks etc.
 source homebrew.sh
+
+# Configure shell and preferences
+# source shell.sh
 
 # Install latest Node and set as default
 nvm install --lts
--- mac-os.sh
+++ mac-os.sh
@@ -5,16 +5,6 @@ echo "Configuring MacBook preferences..."
 # Close any open System Preferences panes to prevent them from overriding settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
 
-# Ask for the administrator password upfront
-# sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished
-# while true; do
-#   sudo -n true
-#   sleep 60
-#   kill -0 "$$" || exit
-# done 2>/dev/null &
-
 ################################################################################
 # region - General UI/UX                                                       #
 ################################################################################
