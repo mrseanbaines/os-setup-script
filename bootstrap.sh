f198f4293717d8e56aec57b5ec2bd238553c569f
Sean Baines <mrseanbaines@gmail.com>
2026-04-14 15:10:17 +0100

    Restore sudo keep-alive and shell configuration step in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,14 +3,14 @@
 echo "Starting setup..."
 
 # Ask for the administrator password upfront
-# sudo -v
+sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished
-# while true; do
-#   sudo -n true
-#   sleep 60
-#   kill -0 "$$" || exit
-# done 2>/dev/null &
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
 
 # Install nvm & Node
 source node.sh
@@ -22,6 +22,6 @@ source mac-os.sh
 source homebrew.sh
 
 # Configure shell and preferences
-# source shell.sh
+source shell.sh
 
 echo "MacBook setup complete! 🎉"
