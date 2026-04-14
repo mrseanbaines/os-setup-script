85e4e62d0dc4fba62818ecc2baeca2fac7497962
Sean Baines <mrseanbaines@gmail.com>
2026-04-14 17:26:02 +0100

    Install zsh-syntax-highlighting and zsh-autosuggestions Oh My Zsh plugins

--- shell.sh
+++ shell.sh
@@ -12,6 +12,17 @@ if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
+# Install Oh My Zsh plugins (skip if already installed)
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+fi
+
+if [[ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+fi
+
 # Download dotfiles
 curl -fsSL https://raw.githubusercontent.com/mrseanbaines/dotfiles/master/.zshrc -o ~/.zshrc
 curl -fsSL https://raw.githubusercontent.com/mrseanbaines/dotfiles/master/.gitconfig -o ~/.gitconfig
