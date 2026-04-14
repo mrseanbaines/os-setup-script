12d4e53975ceb9c5ee2514afe0123212cb60c91c
Sean Baines <mrseanbaines@gmail.com>
2026-04-14 15:33:38 +0100

    Fix n permission error by setting N_PREFIX to a user-owned directory
    By default n installs Node to /usr/local/n which requires sudo. Setting
    N_PREFIX to ~/.n avoids the permission issue entirely.

--- node.sh
+++ node.sh
@@ -2,6 +2,10 @@
 
 echo "Running Node steps..."
 
+# Install Node to a user-owned directory to avoid requiring sudo
+export N_PREFIX="$HOME/.n"
+export PATH="$N_PREFIX/bin:$PATH"
+
 # Install latest LTS Node
 n lts
 
