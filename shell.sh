233e35fb1ab891241849b8a904775288e2f831b1
Sean Baines <mrseanbaines@gmail.com>
2026-04-14 15:09:52 +0100

    Fix Oh My Zsh never being installed in shell.sh
    exec $SHELL replaced the running process before the Oh My Zsh installer
    could run. Removed the exec call — chsh already sets zsh as the default
    for the next login session.

--- shell.sh
+++ shell.sh
@@ -5,10 +5,6 @@ echo "Configuring shell..."
 # Set zsh as the default shell
 chsh -s $(which zsh)
 
-# Reload the shell to start using zsh
-# exec $(which zsh)
-exec $SHELL
-
 # Install Oh My Zsh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
