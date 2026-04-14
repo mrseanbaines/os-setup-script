bc83a39b58dc95e2a4e68d056369af238279d01e
Sean Baines <mrseanbaines@gmail.com>
2026-04-14 15:09:20 +0100

    Fix nvm not available after install in node.sh
    The nvm installer only adds nvm to shell config files (.bashrc/.zshrc),
    so it isn't available in the current session. Source it explicitly so
    that nvm install --lts doesn't fail with "command not found".
    
    Also add missing shebang.

--- node.sh
+++ node.sh
@@ -1,7 +1,13 @@
+#!/usr/bin/env bash
+
 echo "Running Node steps..."
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+# Source nvm into the current session (the installer only adds it to shell config files)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Install latest Node and set as default
 nvm install --lts
